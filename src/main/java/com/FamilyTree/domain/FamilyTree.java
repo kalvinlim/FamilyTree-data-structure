cb452ae3c4c9223c0c63e3f841e7bdee84b5f8d9
Kalvin <Kalvin@foobar.com>
2015-07-20 01:00:24 -0500

    comments

--- src/main/java/com/FamilyTree/domain/FamilyTree.java
+++ src/main/java/com/FamilyTree/domain/FamilyTree.java
@@ -56,11 +56,22 @@ public class FamilyTree implements Tree {
 	public List<Node> getNodesWithNoSiblings() {
 		return searchNodesWithNoSiblings(root, new ArrayList<Node>());
 	}
+	
+	/**
+	 * Recursive search of {@link FamilyTree} node collection
+	 * @param currentNode Starter node, typically the root
+	 * @param result List<Node> wrapper to persist search result.  Dirty hack to pass by reference
+	 * and populate with all Node results that have no siblings
+	 * @return List of Nodes that have no siblings
+	 */
 	private List<Node> searchNodesWithNoSiblings(Node currentNode, List<Node> result){		
-		if(currentNode == currentNode.getRoot()){
+		if(currentNode == currentNode.getRoot()){ //Special case to count the root
 			result.add(currentNode);
 		}
 		Node parent = currentNode.getParent();
+		
+		//Determine if currentNode is only child by checking if parent has children 
+		// count == 1
 		if(parent != null && parent.getChildren().size() == 1){
 			result.add(currentNode);
 		}
@@ -76,6 +87,14 @@ public class FamilyTree implements Tree {
 		
 		return searchNodesWithNoChildren(root, result);
 	}
+	
+	/**
+	 * Recursive search of {@link FamilyTree} node collection
+	 * @param currentNode Starter node, typically the root
+	 * @param result List<Node> wrapper to persist search result.  Dirty hack to pass by reference
+	 * and populate with all Node results that have no children
+	 * @return List of Nodes that have no children
+	 */
 	private List<Node> searchNodesWithNoChildren(Node currentNode, List<Node> result){
 		if(currentNode.getChildren().isEmpty()){
 			result.add(currentNode);
@@ -92,19 +111,33 @@ public class FamilyTree implements Tree {
 		return searchNodeWithMostGrandChildren(root, result);
 	}
 	
+	/**
+	 * Recursive search of {@link FamilyTree} node collection
+	 * @param currentNode Starter node, typically the root
+	 * @param result List<Node> wrapper to persist search result.  Dirty hack to pass by reference
+	 * and populate with all Node results that have no children
+	 * @return Node that has the highest grand children count
+	 */
 	private Node searchNodeWithMostGrandChildren(Node currentNode, SortedMap<Integer, Node> result){
-		if(!currentNode.getChildren().isEmpty()){
+		if(!currentNode.getChildren().isEmpty()){ //if currentNode has children
 			int counter = 0;
+			
+			//Iterate through every child of this currentNode,
+			// and for every child, count their children.
+			// In other words, get a count of currentNode's grand children
 			for(Node child : currentNode.getChildren()){				
 				counter += child.getChildren().size();
 			}
-			if(counter > 0){
+			if(counter > 0){ // if grand children > 0 , or rather if the node is a grand parent
+				//Append it to ascending SortedMap - (key: grand children count, value: node) 
 				result.put(counter, currentNode);
 			}
 		}				
 		for(Node child : currentNode.getChildren()){	
 			searchNodeWithMostGrandChildren(child, result);
 		}
+		
+		//Return the last node, that is the node with the highest count of grand children
 		return result.isEmpty() ? null : result.get(result.lastKey());		
 	}
 	
@@ -115,7 +148,7 @@ public class FamilyTree implements Tree {
 	}
 
 	/**
-	 * Generate default tree
+	 * Helper function to generate default tree used for demo and testing purposes
 	 * @return "pre-built" family tree for immediate use
 	 */
 	public static FamilyTree generateDefaultTree(){
@@ -152,6 +185,7 @@ public class FamilyTree implements Tree {
 		
 		return new FamilyTree(nancy);
 	}
+	
 	@Override
 	public String drawTree() {
 		// TODO Auto-generated method stub
